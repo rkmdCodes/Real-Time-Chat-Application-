532045753450918fedd8f0b10a5527b18dfc5402
rkmdCodes <113243168+rkmdCodes@users.noreply.github.com>
2023-01-25 18:47:46 +0530

    creating a new user implemented

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const axios = require('axios');
+
 
 app.use(express.json());
 app.use(cors({origin:true}));
@@ -8,7 +10,21 @@ app.use(cors({origin:true}));
 
 app.post('/authenticate',async (req,res)=>{
     const { username } = req.body;
-    return res.json({username:username,secret:'sha256'});
+    const ENDPOINT = 'https://api.chatengine.io/users/'
+
+    try {
+      
+        const rest = await axios.put(ENDPOINT,{
+            username: username, secret: username, first_name: username
+        },
+        {headers: { "Private-Key": "2daa6403-b447-4d88-9c40-660ac30177e9" }}
+        );
+      
+      return res.status(rest.status).json(rest.data);  
+    }
+    catch (error) {
+      return res.status(error.response.status).json(error.response.data);
+    };
 });
 
 
